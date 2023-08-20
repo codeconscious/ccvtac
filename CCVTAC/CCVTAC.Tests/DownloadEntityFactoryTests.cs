b332b2958302e07dfcf8c40fc5c4cfc88c1a0d92
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2023-08-20 20:48:46 +0900

    Fix tests

--- CCVTAC/CCVTAC.Tests/DownloadEntityFactoryTests.cs
+++ CCVTAC/CCVTAC.Tests/DownloadEntityFactoryTests.cs
@@ -10,7 +10,7 @@ public class DownloadEntityFactory
         var videoUrl = "https://www.youtube.com/watch?v=5B1rB894B1U";
         var result = CCVTAC.Console.DownloadEntities.DownloadEntityFactory.Create(videoUrl);
         var expectedType = new Video("5B1rB894B1U").GetType();
-        Assert.Equal(expectedType, result.GetType());
+        Assert.Equal(expectedType, result.Value.GetType());
     }
 
     [Fact]
@@ -19,7 +19,7 @@ public class DownloadEntityFactory
         var playlistUrl = "https://www.youtube.com/playlist?list=PLbGKwbAYGKxKMHoGVLhzoyPnH9rTXEH6z";
         var result = CCVTAC.Console.DownloadEntities.DownloadEntityFactory.Create(playlistUrl);
         var expectedType = new Playlist("PLbGKwbAYGKxKMHoGVLhzoyPnH9rTXEH6z").GetType();
-        Assert.Equal(expectedType, result.GetType());
+        Assert.Equal(expectedType, result.Value.GetType());
     }
 
     [Fact]
@@ -28,41 +28,6 @@ public class DownloadEntityFactory
         var channelUrl = "https://www.youtube.com/channel/UCqLwLsPsfuy_vSNK09sLomw";
         var result = CCVTAC.Console.DownloadEntities.DownloadEntityFactory.Create(channelUrl);
         var expectedType = new Channel("UCqLwLsPsfuy_vSNK09sLomw").GetType();
-        Assert.Equal(expectedType, result.GetType());
+        Assert.Equal(expectedType, result.Value.GetType());
     }
-
-    //[Fact]
-    //public void VideoTest()
-    //{
-    //    var url = "https://www.youtube.com/watch?v=5B1rB894B1U";
-    //    var videoDownload = new Video("5B1rB894B1U");
-    //    var result = videoDownload.DoesMatch(url);
-    //    Assert.True(result);
-    //}
-    //[Fact]
-    //public void VideoTest_Invalid()
-    //{
-    //    var url = "https://www.youtube.com/watch?v=5B1rB894B1";
-    //    var videoDownload = new Video("5B1rB894B1");
-    //    var result = videoDownload.DoesMatch(url);
-    //    Assert.False(result);
-    //}
-
-    //[Fact]
-    //public void PlaylistTest()
-    //{
-    //    var url = "https://www.youtube.com/playlist?list=PLbGKwbAYGKxKMHoGVLhzoyPnH9rTXEH6z";
-    //    var playlistDownload = new Playlist("PLbGKwbAYGKxKMHoGVLhzoyPnH9rTXEH6z");
-    //    var result = playlistDownload.DoesMatch(url);
-    //    Assert.True(result);
-    //}
-
-    //[Fact]
-    //public void ChannelTest()
-    //{
-    //    var url = "https://www.youtube.com/channel/UCqLwLsPsfuy_vSNK09sLomw";
-    //    var channelDownload = new Channel("UCqLwLsPsfuy_vSNK09sLomw");
-    //    var result = channelDownload.DoesMatch(url);
-    //    Assert.True(result);
-    //}
 }
