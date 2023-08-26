fe60738699589394f63d37cf4d60ceff19545b0e
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2023-08-26 17:55:44 +0900

    Move help to its own file

--- CCVTAC/CCVTAC.Console/Program.cs
+++ CCVTAC/CCVTAC.Console/Program.cs
@@ -15,7 +15,7 @@ class Program
 
         if (args.Length > 0 && HelpCommands.Contains(args[0].ToLowerInvariant()))
         {
-            PrintHelp(printer);
+            Help.PrintHelp(printer);
             return;
         }
 
@@ -134,29 +134,5 @@ class Program
             args.Add("--split-chapters");
 
         return string.Join(" ", args.Concat(additionalArgs ?? Enumerable.Empty<string>()));
-    }
-
-    static void PrintHelp(Printer printer)
-    {
-        printer.Print("CodeConscious Video-to-Audio Converter (CCVTAC)");
-        printer.Print("- Easily convert YouTube videos to local audio files with ID3v2 tags!");
-        printer.Print($"  (Supported audio formats: {string.Join(", ", SettingsService.ValidAudioFormats)})");
-        printer.Print("- Supports video and playlist URLs");
-        printer.Print("- Video metadata (uploader name and URL, source URL, etc.) saved to Comment tags");
-        printer.Print("- Auto-renames files via specific regex patterns (to remove resource IDs, etc.)");
-        printer.Print("- Video thumbnails are auto-trimmed and written to files as album art (Optional)");
-        printer.Print("- Post-processed files are automatically moved to a specified directory");
-        printer.Print("- All URLs entered are saved locally to a file named `history.log`",
-                      appendLines: 1);
-
-        printer.Print("Prerequisites:");
-        printer.Print("- [Required] yt-dlp (https://github.com/yt-dlp/yt-dlp/) for downloading audio");
-        printer.Print("- [Optional] mogrify (https://imagemagick.org/script/mogrify.php) for auto-cropping album art",
-                      appendLines: 1);
-
-        printer.Print("Instructions:");
-        printer.Print("• Run the program once to generate a blank settings.json file, then populate it with directory paths.");
-        printer.Print("• After the application starts, enter single video or playlist URLs to start the download process.");
-        printer.Print("• Enter \"q\" or \"quit\" to quit.");
     }
 }
