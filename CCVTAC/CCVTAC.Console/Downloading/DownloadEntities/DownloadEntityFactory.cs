4dcb4ec878d74b12cbea51b5814724efb3ff7f30
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2023-09-01 20:18:28 +0900

    Clearer code

--- CCVTAC/CCVTAC.Console/Downloading/DownloadEntities/DownloadEntityFactory.cs
+++ CCVTAC/CCVTAC.Console/Downloading/DownloadEntities/DownloadEntityFactory.cs
@@ -8,29 +8,29 @@ public static class DownloadEntityFactory
 
     private static readonly Dictionary<DownloadType, Regex> Patterns = new()
     {
-        { DownloadType.Video, new Regex(@"^[\w-]{11}$|(?<=v=|v\\=)[\w-]{11}|(?<=youtu\.be/).{11}") },
+        { DownloadType.Video,    new Regex(@"^[\w-]{11}$|(?<=v=|v\\=)[\w-]{11}|(?<=youtu\.be/).{11}") },
         { DownloadType.Playlist, new Regex(@"(?<=list=)[\w\-]+") },
-        { DownloadType.Channel, new Regex(@"(?:www\.)?youtube\.com/(?:c/|channel/|@|user/)[\w\-]+") },
+        { DownloadType.Channel,  new Regex(@"(?:www\.)?youtube\.com/(?:c/|channel/|@|user/)[\w\-]+") },
     };
 
     public static Result<IDownloadEntity> Create(string url)
     {
-        var a = Patterns
-            .Select(pattern => (pattern.Key, pattern.Value.Match(url)))
-            .Where(typeAndMatch => typeAndMatch.Item2.Success)
-            .Select(typeAndMatch => (typeAndMatch.Item1, typeAndMatch.Item2.Value));
+        var typesWithResourceIds =
+            Patterns.Select(pattern =>      (pattern.Key, pattern.Value.Match(url)))
+                    .Where(typeAndMatch =>  typeAndMatch.Item2.Success)
+                    .Select(typeAndMatch => (typeAndMatch.Key, typeAndMatch.Item2.Value));
 
-        if (!a.Any())
+        if (!typesWithResourceIds.Any())
             return Result.Fail(_unsupportedUrlText);
 
-        var (type, resourceId) = a.First();
+        var (type, resourceId) = typesWithResourceIds.First();
 
         return type switch
         {
             DownloadType.Video =>    Result.Ok((IDownloadEntity) new Video(resourceId)),
             DownloadType.Playlist => Result.Ok((IDownloadEntity) new Playlist(resourceId)),
             DownloadType.Channel=>   Result.Ok((IDownloadEntity) new Channel(resourceId)),
-            _ => Result.Fail(_unsupportedUrlText)
+            _ =>                     Result.Fail(_unsupportedUrlText)
         };
     }
 }
