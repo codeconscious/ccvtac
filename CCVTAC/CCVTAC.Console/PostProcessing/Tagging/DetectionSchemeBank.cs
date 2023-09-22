77b9908e098a88c6a22c0c1a5fa7da286d8329c0
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2023-09-22 17:29:05 +0900

    Use triple quotation mark style for all regex patterns

--- CCVTAC/CCVTAC.Console/PostProcessing/Tagging/DetectionSchemeBank.cs
+++ CCVTAC/CCVTAC.Console/PostProcessing/Tagging/DetectionSchemeBank.cs
@@ -10,19 +10,19 @@ internal static class DetectionSchemeBank
     internal static IReadOnlyList<DetectionScheme> Title = new List<DetectionScheme>()
     {
         new(
-            @"(.+?) · (.+)(?:\n|\r|\r\n){2}(.+)(?:\n|\r|\r\n){2}.*℗ ([12]\d{3})\D",
+            """(.+?) · (.+)(?:\n|\r|\r\n){2}(.+)(?:\n|\r|\r\n){2}.*℗ ([12]\d{3})\D""",
             MatchGroup.Group1,
             SourceField.Description,
             "Topic style"
         ),
         new(
-            @"(.+?) · (.+)(?:\n|\r|\r\n){2}(.+)(?:\n|\r|\r\n){2}.*℗",
+            """(.+?) · (.+)(?:\n|\r|\r\n){2}(.+)(?:\n|\r|\r\n){2}.*℗""",
             MatchGroup.Group1,
             SourceField.Description,
             "pseudo-Topic style"
         ),
         new(
-            @"(.+) (?:\d\w{2}|Vol\.\d)?『(.+)』\[([12]\d{3})\]",
+            """(.+) (?:\d\w{2}|Vol\.\d)?『(.+)』\[([12]\d{3})\]""",
             MatchGroup.Group2,
             SourceField.Title
         ),
@@ -41,19 +41,19 @@ internal static class DetectionSchemeBank
     internal static IReadOnlyList<DetectionScheme> Artist = new List<DetectionScheme>()
     {
         new (
-            @"(.+?) · (.+)(?:\n|\r|\r\n){2}(.+)(?:\n|\r|\r\n){2}.*℗ ([12]\d{3})\D",
+            """(.+?) · (.+)(?:\n|\r|\r\n){2}(.+)(?:\n|\r|\r\n){2}.*℗ ([12]\d{3})\D""",
             MatchGroup.Group2,
             SourceField.Description,
             "Topic style"
         ),
         new (
-            @"(.+?) · (.+)(?:\n|\r|\r\n){2}(.+)(?:\n|\r|\r\n){2}.*℗",
+            """(.+?) · (.+)(?:\n|\r|\r\n){2}(.+)(?:\n|\r|\r\n){2}.*℗""",
             MatchGroup.Group2,
             SourceField.Description,
             "pseudo-Topic style"
         ),
         new (
-            @"(.+)(?: - )?[「『](.+)[」』]\[([12]\d{3})\]",
+            """(.+)(?: - )?[「『](.+)[」』]\[([12]\d{3})\]""",
             MatchGroup.Group1,
             SourceField.Title
         ),
@@ -72,18 +72,18 @@ internal static class DetectionSchemeBank
     internal static IReadOnlyList<DetectionScheme> Album = new List<DetectionScheme>()
     {
         new (
-            @"(?<=[Aa]lbum: ).+",
+            """(?<=[Aa]lbum: ).+""",
             MatchGroup.Group0,
             SourceField.Description
         ),
         new (
-            @"(.+?) · (.+)(?:\n|\r|\r\n){2}(.+)(?:\n|\r|\r\n){2}.*℗ ([12]\d{3})\D",
+            """(.+?) · (.+)(?:\n|\r|\r\n){2}(.+)(?:\n|\r|\r\n){2}.*℗ ([12]\d{3})\D""",
             MatchGroup.Group3,
             SourceField.Description,
             "Topic style"
         ),
         new (
-            @"(.+?) · (.+)(?:\n|\r|\r\n){2}(.+)(?:\n|\r|\r\n){2}.*℗",
+            """(.+?) · (.+)(?:\n|\r|\r\n){2}(.+)(?:\n|\r|\r\n){2}.*℗""",
             MatchGroup.Group3,
             SourceField.Description,
             "pseudo-Topic style"
@@ -108,12 +108,12 @@ internal static class DetectionSchemeBank
     internal static IReadOnlyList<DetectionScheme> Composers = new List<DetectionScheme>()
     {
         new (
-            @"(?<=[Cc]omposed by |[Cc]omposed by: |[Cc]omposer: |作曲[:：]).+",
+            """(?<=[Cc]omposed by |[Cc]omposed by: |[Cc]omposer: |作曲[:：]).+""",
             MatchGroup.Group0,
             SourceField.Description
         ),
         new (
-            @"(?<=[Cc]omposed by |[Cc]omposed by: |[Cc]omposer: |作曲[:：]).+",
+            """(?<=[Cc]omposed by |[Cc]omposed by: |[Cc]omposer: |作曲[:：]).+""",
             MatchGroup.Group0,
             SourceField.Title
         )
@@ -122,41 +122,41 @@ internal static class DetectionSchemeBank
     internal static IReadOnlyList<DetectionScheme> Year = new List<DetectionScheme>()
     {
         new (
-            @"(?<=[(（\[［【])[12]\d{3}(?=[)）\]］】])",
+            """(?<=[(（\[［【])[12]\d{3}(?=[)）\]］】])""",
             MatchGroup.Group0,
             SourceField.Title
         ),
         new (
-            @"(?<=℗ )[12]\d{3}(?=\s)",
+            """(?<=℗ )[12]\d{3}(?=\s)""",
             MatchGroup.Group0,
             SourceField.Description,
             "℗\" symbol"
         ),
         new (
-            @"(?<=[Rr]eleased [io]n: )[12]\d{3}",
+            """(?<=[Rr]eleased [io]n: )[12]\d{3}""",
             MatchGroup.Group0,
             SourceField.Description,
             "'released on' date"
         ),
         new (
-            @"[12]\d{3}(?=(?:[.\/年]\d{1,2}[.\/月]\d{1,2}日?\s?)?\s?(?:[Rr]elease|リリース|発売))",
+            """[12]\d{3}(?=(?:[.\/年]\d{1,2}[.\/月]\d{1,2}日?\s?)?\s?(?:[Rr]elease|リリース|発売))""",
             MatchGroup.Group0,
             SourceField.Description,
             "'year first'-style year"
         ),
         new (
-            @"[12]\d{3}年(?=\d{1,2}月\d{1,2}日\s?[Rr]elease)",
+            """[12]\d{3}年(?=\d{1,2}月\d{1,2}日\s?[Rr]elease)""",
             MatchGroup.Group0,
             SourceField.Description,
             "description's 年月日-style release date"
         ),
         new (
-            @"(.+) (?:\d\w{2}|Vol\.\d)?『(.+)』\[([12]\d{3})\]",
+            """(.+) (?:\d\w{2}|Vol\.\d)?『(.+)』\[([12]\d{3})\]""",
             MatchGroup.Group3,
             SourceField.Title
         ),
         new (
-            @"(.+) (?:\d\w{2}|Vol\.\d)?『(.+)』\[([12]\d{3})\]",
+            """(.+) (?:\d\w{2}|Vol\.\d)?『(.+)』\[([12]\d{3})\]""",
             MatchGroup.Group3,
             SourceField.Description
         )
