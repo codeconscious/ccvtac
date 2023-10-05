f3bf044c818a2153c3b3b164d4d3205f8ae926a0
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2023-10-05 14:16:51 +0900

    Variable updates

--- CCVTAC/CCVTAC.Console/IoUtilties/Directories.cs
+++ CCVTAC/CCVTAC.Console/IoUtilties/Directories.cs
@@ -9,14 +9,13 @@ internal static class Directories
     /// </summary>
     /// <param name="workingDirectory"></param>
     /// <param name="customIgnoreFiles">An optional list of files to be excluded.</param>
-    internal static ImmutableList<string> GetDirectoryFiles(
-        string workingDirectory,
-        IEnumerable<string>? customIgnoreFiles = null)
+    internal static ImmutableList<string> GetDirectoryFiles(string workingDirectory,
+                                                            IEnumerable<string>? customIgnoreFiles = null)
     {
         var ignoreFiles = customIgnoreFiles?.Distinct() ?? Enumerable.Empty<string>();
 
         return Directory.GetFiles(workingDirectory, "*", new EnumerationOptions())
-                        .Where(dirFile => !ignoreFiles.Any(ignoreFile => dirFile.EndsWith(ignoreFile)))
+                        .Where(dirFilePath => !ignoreFiles.Any(ignoreFile => dirFilePath.EndsWith(ignoreFile)))
                         .ToImmutableList();
     }
 }
