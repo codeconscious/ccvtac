ba9b4d27bdc6f0ed9249dce13d62a1cecbb2ce0e
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2023-11-26 16:53:44 +0900

    Add VideoOnPlaylist test

--- CCVTAC/CCVTAC.Tests/DownloadEntityFactoryTests.cs
+++ CCVTAC/CCVTAC.Tests/DownloadEntityFactoryTests.cs
@@ -7,27 +7,36 @@ public sealed class DownloadEntityFactoryTests
     [Fact]
     public void Video()
     {
-        string videoUrl = "https://www.youtube.com/watch?v=5B1rB894B1U";
-        var videoResult = DownloadEntityFactory.Create(videoUrl);
-        MediaDownloadType expectedType = new Video("5B1rB894B1U").VideoDownloadType;
-        Assert.Equal(expectedType, videoResult.Value.VideoDownloadType);
+        string url = "https://www.youtube.com/watch?v=5B1rB894B1U";
+        var creationResult = DownloadEntityFactory.Create(url);
+        MediaDownloadType expected = new Video("5B1rB894B1U").VideoDownloadType;
+        Assert.Equal(expected, creationResult.Value.VideoDownloadType);
+    }
+
+    [Fact]
+    public void VideoOnPlaylist()
+    {
+        string url = "https://www.youtube.com/watch?v=3pG1MvwcRUh&list=LAK5uy_kgsbf_bzaknqCjNbb2BtnfylIvHdNlKzg&index=1";
+        var creationResult = DownloadEntityFactory.Create(url);
+        MediaDownloadType expected = new VideoOnPlaylist("3pG1MvwcRUh", "LAK5uy_kgsbf_bzaknqCjNbb2BtnfylIvHdNlKzg").VideoDownloadType;
+        Assert.Equal(expected, creationResult.Value.VideoDownloadType);
     }
 
     [Fact]
     public void Playlist()
     {
-        string playlistUrl = "https://www.youtube.com/playlist?list=PLbGKwbAYGKxKMHoGVLhzoyPnH9rTXEH6z";
-        var playlistResult = DownloadEntityFactory.Create(playlistUrl);
-        MediaDownloadType expectedType = new Playlist("PLbGKwbAYGKxKMHoGVLhzoyPnH9rTXEH6z").VideoDownloadType;
-        Assert.Equal(expectedType, playlistResult.Value.VideoDownloadType);
+        string url = "https://www.youtube.com/playlist?list=PLbGKwbAYGKxKMHoGVLhzoyPnH9rTXEH6z";
+        var creationResult = DownloadEntityFactory.Create(url);
+        MediaDownloadType expected = new Playlist("PLbGKwbAYGKxKMHoGVLhzoyPnH9rTXEH6z").VideoDownloadType;
+        Assert.Equal(expected, creationResult.Value.VideoDownloadType);
     }
 
     [Fact]
     public void Channel()
     {
         string channelUrl = "https://www.youtube.com/channel/UCqLwLsPsfuy_vSNK09sLomw";
-        var channelResult = DownloadEntityFactory.Create(channelUrl);
-        MediaDownloadType expectedType = new Channel("UCqLwLsPsfuy_vSNK09sLomw").VideoDownloadType;
-        Assert.Equal(expectedType, channelResult.Value.VideoDownloadType);
+        var creationResult = DownloadEntityFactory.Create(channelUrl);
+        MediaDownloadType expected = new Channel("UCqLwLsPsfuy_vSNK09sLomw").VideoDownloadType;
+        Assert.Equal(expected, creationResult.Value.VideoDownloadType);
     }
 }
