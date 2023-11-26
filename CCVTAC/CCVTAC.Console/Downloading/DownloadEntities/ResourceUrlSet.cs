42293ec4e90fac9c3c6b98a8b93854c5069c3959
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2023-11-26 16:44:50 +0900

    Add comments

--- CCVTAC/CCVTAC.Console/Downloading/DownloadEntities/ResourceUrlSet.cs
+++ CCVTAC/CCVTAC.Console/Downloading/DownloadEntities/ResourceUrlSet.cs
@@ -1,9 +1,23 @@
 namespace CCVTAC.Console.Downloading.DownloadEntities;
 
-public class ResourceUrlSet
+/// <summary>
+/// The set of data necessary to create URLs for resources.
+/// </summary>
+public sealed record ResourceUrlSet
 {
+    /// <summary>
+    /// The base part of the URL, which precedes the resource ID in the full URL.
+    /// </summary>
     public string UrlBase { get; init; }
+
+    /// <summary>
+    /// An ID to a specific resource (e.g., video, playlist, or channel).
+    /// </summary>
     public string ResourceId { get; init; }
+
+    /// <summary>
+    /// A complete URL to a resource, suitable to passing to other programs.
+    /// </summary>
     public string FullResourceUrl => UrlBase + ResourceId;
 
     public ResourceUrlSet(string urlBase, string resourceId)
