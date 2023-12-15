9aeffdc6ef674a6f38ee3e5f787d9518cb4f8fb0
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2023-12-15 18:44:12 +0900

    Add F# project

---
+++ CCVTAC/CCVTAC.FSharp/Library.fs
@@ -0,0 +1,21 @@
+﻿namespace CCVTAC.FSharp
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
+
+module YouTube =
+    type MediaId = { Id: string }
+    type SingleId = { MediaId: MediaId }
+    type IdPair = { PrimaryId: MediaId; SupplementaryId: MediaId }
+
+    type MediaType =
+        | PlaylistVideo of resourcePair: IdPair
+        | Video of resourceId: SingleId
+        | StandardPlaylist of resourceId: SingleId
+        | ReleasePlaylist of resourceId: SingleId
+        | Channel of resourceId: SingleId
+
+    type DownloadType =
+        | MediaType of MediaType
+        | Metadata
