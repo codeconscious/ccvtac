945613ba3848a17e7a00d9e4f2072fd95f518349
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2024-02-02 20:41:37 +0900

    In progress

--- CCVTAC/CCVTAC.FSharp/Library.fs
+++ CCVTAC/CCVTAC.FSharp/Library.fs
@@ -5,15 +5,24 @@ module Say =
         printfn "Hello %s" name
 
 module YouTube =
-    type MediaId = { VideoId: string }
-    type MediaIdPair = { VideoId: string; ParentId: string }
+    open System.Text.RegularExpressions
+
+    // type Id = { VideoId: string }
+    type Id = Id of string
+    let CreateId (s:string) =
+        if System.Text.RegularExpressions.Regex.IsMatch(s,@".{11}")
+            then Some (Id s)
+            else None
+
+    type IdPair = { VideoId: string; ParentId: string }
 
     type Media =
-        | Video of MediaId
-        | PlaylistVideo of MediaIdPair
-        | StandardPlaylist of MediaIdPair
-        | ReleasePlaylist of MediaIdPair
-        | Channel of MediaIdPair
+        | Video of Id
+        | PlaylistVideo of IdPair
+        | StandardPlaylist of Id
+        | ReleasePlaylist of Id
+        | Channel of Id
+        | Unknown
 
     type DownloadType =
         | MediaType of Media
@@ -26,6 +35,7 @@ module YouTube =
         | StandardPlaylist _ -> "https://www.youtube.com/playlist?list="
         | ReleasePlaylist _ -> "https://www.youtube.com/playlist?list="
         | Channel _ -> "https://"
+        | _ -> ""
 
     let fullUrl (media:Media) =
         urlBase media + match media with
@@ -34,3 +44,23 @@ module YouTube =
                         | StandardPlaylist data -> data.VideoId
                         | ReleasePlaylist data -> data.VideoId
                         | Channel data -> data.VideoId
+                        | Unknown -> failwith "Not Implemented"
+
+    // Source: https://stackoverflow.com/questions/53818476/f-match-many-regex
+    // Reference: https://learn.microsoft.com/en-us/dotnet/fsharp/language-reference/active-patterns
+    // This is an "active recognizer."
+    let (|Regex|_|) pattern input =
+        let m = Regex.Match(input, pattern)
+        if m.Success then Some(List.tail [ for g in m.Groups -> g.Value ])
+        else None
+
+    let getMedia (url:string) =
+        match url with
+        | Regex "^([\w-]{11})$" [ id ] -> Video({VideoId = id})
+        | Regex "(?<=v=|v\\=)([\w-]{11})" [ id ] -> Video({VideoId = id})
+        | Regex "(?<=youtu\.be/)(.{11})" [ id ] -> Video({VideoId = id})
+        | Regex "(?<=v=|v\=)([\w-]{11})(?:&list=([\w_-]+))" [ id ; parentId] -> PlaylistVideo({VideoId = id; ParentId = parentId})
+        | Regex "(?<=list=)(P[\w\-]+)" [ id ] -> StandardPlaylist({VideoId = id})
+        | Regex "(?<=list=)(O[\w\-]+)" [ id ] -> ReleasePlaylist({VideoId = id})
+        | Regex "((?:www\.)?youtube\.com\/(?:channel\/|c\/|user\/|@)(?:[\w\-]+))" [ id ] -> Channel({VideoId = id})
+        | _ -> Unknown
