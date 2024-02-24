60720755c7c783af6b44af143721ad7003f3c4db
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2024-02-24 13:43:21 +0900

    Updates

--- CCVTAC/CCVTAC.FSharp/Library.fs
+++ CCVTAC/CCVTAC.FSharp/Library.fs
@@ -1,65 +1,89 @@
 ﻿namespace CCVTAC.FSharp
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
-
 module YouTube =
     open System.Text.RegularExpressions
+
+    type Url = Url of string
 
     // type VideoId = VideoId of string
     // type PlaylistId = PlaylistId of string
     // type ChannelId = ChannelId of string
-    type Url = Url of string
-    // type Id = Id of string
-    type MediaType = | Video | PlaylistVideo | StandardPlaylist | ReleasePlaylist | Channel | Unknown
-    type IdPair = { VideoId: string; PlaylistId: string }
 
-    let parseUrl (s:string) (rgx, mediaType) =
-        let matches = System.Text.RegularExpressions.Regex.Matches(s, rgx)
-        if matches.Count > 1
-        then Some (matches |> Seq.cast |> Seq.filter (fun (regMatch:Match) -> regMatch.Success) |> Seq.map (fun (regMatch:Match) -> regMatch.Value))
-        else None
+    // type MediaId =
+    //     | VideoId
+    //     | PlaylistId
+    //     | ChannelId
 
-    //  let unwrap (Media media) = media
+    type MediaType =
+        | Video
+        | PlaylistVideo
+        | StandardPlaylist
+        | ReleasePlaylist
+        | Channel
+        | Unknown
 
-    type DownloadType =
-        | MediaType of MediaType
-        | Metadata
-
-    let urlPattern (media:MediaType) (url:string) =
-        match media with
-        | Video str -> sprintf "https://www.youtube.com/watch?v=%s" str
-        | PlaylistVideo -> "https://www.youtube.com/watch?v=%s"
-        | StandardPlaylist -> "https://www.youtube.com/playlist?list=%s"
-        | ReleasePlaylist -> "https://www.youtube.com/playlist?list=%s"
-        | Channel -> "https://"
-        | _ -> ""
-
-    let fullUrl (media: Media) =
-        urlBase media + match media with
-                        | Video id -> id
-                        | PlaylistVideo (videoId, playlistId) -> videoId + playlistId // TODO: Fix
-                        | StandardPlaylist id -> id
-                        | ReleasePlaylist id -> id
-                        | Channel id -> id
-                        | Unknown -> failwith "Not Implemented"
+    // type DownloadType =
+    //     | MediaType of MediaType
+    //     | Metadata
 
     // Source: https://stackoverflow.com/questions/53818476/f-match-many-regex
     // Reference: https://learn.microsoft.com/en-us/dotnet/fsharp/language-reference/active-patterns
+    // Reference: https://jason-down.com/2017/01/24/f-pattern-matching-part-2-active-patterns/
     // This is an "active recognizer."
-    let (|Regex|_|) pattern input =
+    let (|Regex|_|) input pattern =
         let m = Regex.Match(input, pattern)
         if m.Success then Some(List.tail [ for g in m.Groups -> g.Value ])
         else None
 
-    // let getMedia (url:string) =
-    //     match url with
-    //     | Regex "^([\w-]{11})$" [ id ] -> Video({VideoId = id})
-    //     | Regex "(?<=v=|v\\=)([\w-]{11})" [ id ] -> Video({VideoId = id})
-    //     | Regex "(?<=youtu\.be/)(.{11})" [ id ] -> Video({VideoId = id})
-    //     | Regex "(?<=v=|v\=)([\w-]{11})(?:&list=([\w_-]+))" [ id ; parentId] -> PlaylistVideo({VideoId = id; ParentId = parentId})
-    //     | Regex "(?<=list=)(P[\w\-]+)" [ id ] -> StandardPlaylist({VideoId = id})
-    //     | Regex "(?<=list=)(O[\w\-]+)" [ id ] -> ReleasePlaylist({VideoId = id})
-    //     | Regex "((?:www\.)?youtube\.com\/(?:channel\/|c\/|user\/|@)(?:[\w\-]+))" [ id ] -> Channel({VideoId = id})
-    //     | _ -> Unknown
+    let mediaTypeWithUrls (realUrl:Url) =
+        let (Url url) = realUrl
+        match url with
+        | Regex "^([\w-]{11})$" [ id ] -> (Video, [id])
+        | Regex "(?<=v=|v\\=)([\w-]{11})" [ id ] -> (Video, [id])
+        | Regex "(?<=youtu\.be/)(.{11})" [ id ] -> (Video, [id])
+        | Regex "(?<=v=|v\=)([\w-]{11})(?:&list=([\w_-]+))" [ id ; parentId]
+            -> (PlaylistVideo, [id; parentId])
+        | Regex "(?<=list=)(P[\w\-]+)" [ id ] -> (StandardPlaylist, [id])
+        | Regex "(?<=list=)(O[\w\-]+)" [ id ] -> (ReleasePlaylist, [id])
+        | Regex "((?:www\.)?youtube\.com\/(?:channel\/|c\/|user\/|@)(?:[\w\-]+))" [ id ] -> (Channel, [id])
+        | _ -> (Unknown, [])
+        // | failwith "The URL didn't match any registered URL pattern..."
+
+    // let parseUrl (url1:Url) (rgx, mediaType) =
+    //     let (Url url2) = url1
+    //     let matches = System.Text.RegularExpressions.Regex.Matches(url2, rgx)
+    //     if matches.Count = 0
+    //     then None
+    //     else Some (matches
+    //                |> Seq.cast
+    //                |> Seq.filter (fun (regMatch:Match) -> regMatch.Success)
+    //                |> Seq.map (fun (regMatch:Match) -> regMatch.Value))
+
+    let createCleanUrls (mediaType, ids:string list) =
+        // let id = ids.[0]
+        match mediaType with
+        | Video when ids.Length = 1
+            -> Some ([sprintf "https://www.youtube.com/watch?v=%s" ids.[0]])
+        | PlaylistVideo when ids.Length = 2
+            -> Some ([sprintf "https://www.youtube.com/watch?v=%s" ids.[0];
+                      sprintf "https://www.youtube.com/playlist?list=%s" ids.[1]])
+        | StandardPlaylist | ReleasePlaylist
+            -> Some ([sprintf "https://www.youtube.com/playlist?list=%s" ids.[0]])
+        | Channel
+            -> Some ([sprintf "https://%s" ids.[0]])
+        | _ -> None
+        // | failwith "An invalid count of ids was passed for the media type."
+
+    // let fullUrl (media: Media) =
+    //     urlBase media + match media with
+    //                     | Video id -> id
+    //                     | PlaylistVideo (videoId, playlistId) -> videoId + playlistId // TODO: Fix
+    //                     | StandardPlaylist id -> id
+    //                     | ReleasePlaylist id -> id
+    //                     | Channel id -> id
+    //                     | Unknown -> failwith "Not Implemented"
+
+    let generateUrlsFromInput rawUrl =
+        let inputUrl = Url rawUrl
+        let urls = mediaTypeWithUrls inputUrl |> createCleanUrls
+        urls
