c05887d8791636c6f4870a153d1e510232e96db1
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2024-03-23 17:47:46 +0900

    Move settings summary to F# library

--- CCVTAC/CCVTAC.Console/Settings/SettingsAdapter.cs
+++ CCVTAC/CCVTAC.Console/Settings/SettingsAdapter.cs
@@ -1,6 +1,4 @@
-using System.IO;
 using Spectre.Console;
-using CCVTAC.FSharp.Settings;
 using UserSettings = CCVTAC.FSharp.Settings.UserSettings;
 
 namespace CCVTAC.Console.Settings;
@@ -21,7 +19,7 @@ public class SettingsAdapter
     /// <remarks>This is intended to be a temporary solution until more code is moved to F#.</remarks>
     internal static Result<UserSettings> ProcessSettings(string? maybeSettingsPath, Printer printer)
     {
-        var path = FilePath.NewFilePath(maybeSettingsPath);
+        var path = FSharp.Settings.FilePath.NewFilePath(maybeSettingsPath);
 
         if (FSharp.Settings.IO.FileExists(path) is { IsOk: true })
         {
@@ -68,10 +66,6 @@ public class SettingsAdapter
         if (header.HasText())
             printer.Print(header!);
 
-        string historyFileNote = File.Exists(settings.HistoryFile)
-            ? "exists"
-            : "will be created";
-
         var table = new Table();
         table.Expand();
         table.Border(TableBorder.HeavyEdge);
@@ -80,42 +74,10 @@ public class SettingsAdapter
         table.HideHeaders();
         table.Columns[1].Width = 100; // Ensure maximum width.
 
-        var settingPairs =
-            new Dictionary<string, string>()
-                {
-                    { $"Split video chapters", settings.SplitChapters ? "ON" : "OFF" },
-                    {
-                        $"Sleep between batches",
-                        $"{settings.SleepSecondsBetweenBatches} {PluralizeIfNeeded("second", settings.SleepSecondsBetweenBatches)}"
-                    },
-                    {
-                        $"Sleep between downloads",
-                        $"{settings.SleepSecondsBetweenDownloads} {PluralizeIfNeeded("second", settings.SleepSecondsBetweenDownloads)}"
-                    },
-                    {
-                        $"Ignore-upload-year channels",
-                        $"{settings.IgnoreUploadYearUploaders?.Length.ToString() ?? "No"} {PluralizeIfNeeded("channel", settings.IgnoreUploadYearUploaders?.Length ?? 0)}"
-                    },
-                    { "Verbose mode", settings.VerboseOutput ? "ON" : "OFF" },
-                    { "Working directory", settings.WorkingDirectory ?? "None found" }, // TODO: Add validation, then remove the null case.
-                    { "Move-to directory", settings.MoveToDirectory ?? "None found" },
-                    { "History log file", $"{settings.HistoryFile?? "None found" } ({historyFileNote})" },
-                }
-            .ToImmutableList();
-        settingPairs.ForEach(pair => table.AddRow(pair.Key, pair.Value));
+        var settingPairs = FSharp.Settings.Summarize(settings);
+        settingPairs.ToList().ForEach(pair =>
+            table.AddRow(pair.Item1, pair.Item2));
 
         printer.Print(table);
-
-        static string PluralizeIfNeeded(string singularTerm, int count)
-        {
-            return (singularTerm, count) switch
-            {
-                { singularTerm: "second", count: 1 } => singularTerm,
-                { singularTerm: "second", count: _ } => "seconds",
-                { singularTerm: "channel", count: 1 } => singularTerm,
-                { singularTerm: "channel", count: _ } => "channels",
-                _ => singularTerm
-            };
-        }
     }
 }
--- CCVTAC/CCVTAC.FSharp/Settings.fs
+++ CCVTAC/CCVTAC.FSharp/Settings.fs
@@ -1,93 +1,112 @@
-namespace CCVTAC.FSharp.Settings
+namespace CCVTAC.FSharp
 
-open System.Text.Json.Serialization
+module Settings =
+    open System.Text.Json.Serialization
 
-// TODO: Make proper modules and ctors?
-// type DirectoryName = DirectoryName of string
-type FilePath = FilePath of string
+    // TODO: Make proper modules and ctors?
+    // type DirectoryName = DirectoryName of string
+    type FilePath = FilePath of string
 
-type UserSettings = {
-    [<JsonPropertyName("workingDirectory")>]             WorkingDirectory: string
-    [<JsonPropertyName("moveToDirectory")>]              MoveToDirectory: string
-    [<JsonPropertyName("historyFile")>]                  HistoryFile: string
-    [<JsonPropertyName("historyDisplayCount")>]          HistoryDisplayCount: byte
-    [<JsonPropertyName("splitChapters")>]                SplitChapters: bool
-    [<JsonPropertyName("sleepSecondsBetweenDownloads")>] SleepSecondsBetweenDownloads: uint16
-    [<JsonPropertyName("sleepSecondsBetweenBatches")>]   SleepSecondsBetweenBatches: uint16
-    [<JsonPropertyName("verboseOutput")>]                VerboseOutput: bool
-    [<JsonPropertyName("ignoreUploadYearUploaders")>]    IgnoreUploadYearUploaders: string array
-}
+    type UserSettings = {
+        [<JsonPropertyName("workingDirectory")>]             WorkingDirectory: string
+        [<JsonPropertyName("moveToDirectory")>]              MoveToDirectory: string
+        [<JsonPropertyName("historyFile")>]                  HistoryFile: string
+        [<JsonPropertyName("historyDisplayCount")>]          HistoryDisplayCount: byte
+        [<JsonPropertyName("splitChapters")>]                SplitChapters: bool
+        [<JsonPropertyName("sleepSecondsBetweenDownloads")>] SleepSecondsBetweenDownloads: uint16
+        [<JsonPropertyName("sleepSecondsBetweenBatches")>]   SleepSecondsBetweenBatches: uint16
+        [<JsonPropertyName("verboseOutput")>]                VerboseOutput: bool
+        [<JsonPropertyName("ignoreUploadYearUploaders")>]    IgnoreUploadYearUploaders: string array
+    }
 
-module IO =
-    open System
-    open System.IO
-    open System.Text.Json
-    open System.Text.Unicode
-    open System.Text.Encodings.Web
+    [<CompiledName("Summarize")>]
+    let summarize settings =
+        let pluralize label count =
+            if count = 1
+            then $"{count} {label}"
+            else $"{count} {label}s"
 
-    [<CompiledName("FileExists")>]
-    let fileExists filePath =
-        let (FilePath file) = filePath
-        match file |> File.Exists with
-        | true -> Ok()
-        | false -> Error($"The file \"path\" does not exist.")
+        [
+            ("Split video chapters", if settings.SplitChapters then "ON" else "OFF")
+            ("Verbose mode", if settings.VerboseOutput then "ON" else "OFF")
+            ("Sleep between batches", settings.SleepSecondsBetweenBatches |> int |> pluralize  "second")
+            ("Sleep between downloads", settings.SleepSecondsBetweenDownloads |> int |> pluralize "second")
+            ("Ignore-upload-year channels", settings.IgnoreUploadYearUploaders.Length |> pluralize "channel")
+            ("Working directory", settings.WorkingDirectory)
+            ("Move-to directory", settings.MoveToDirectory)
+            ("History log file", settings.HistoryFile)
+        ]
 
-    [<CompiledName("Read")>]
-    let read filePath =
-        let (FilePath path) = filePath
+    module IO =
+        open System
+        open System.IO
+        open System.Text.Json
+        open System.Text.Unicode
+        open System.Text.Encodings.Web
 
-        let verify (settings:UserSettings) =
-            let isEmpty str = String.IsNullOrWhiteSpace str
-            let dirMissing str = not <| (Directory.Exists str)
+        [<CompiledName("FileExists")>]
+        let fileExists filePath =
+            let (FilePath file) = filePath
+            match file |> File.Exists with
+            | true -> Ok()
+            | false -> Error($"The file \"path\" does not exist.")
 
-            match settings with
-            | { WorkingDirectory = w } when isEmpty w -> Error $"No working directory was specified."
-            | { WorkingDirectory = w } when dirMissing w -> Error $"Working directory \"{w}\" is missing."
-            | { MoveToDirectory = m } when isEmpty m -> Error $"No move-to directory was specified."
-            | { MoveToDirectory = m } when dirMissing m -> Error $"Move-to directory \"{m}\" is missing."
-            | _ -> Ok settings
+        [<CompiledName("Read")>]
+        let read filePath =
+            let (FilePath path) = filePath
 
-        try
-            path
-            |> File.ReadAllText
-            |> JsonSerializer.Deserialize<UserSettings>
-            |> verify
-        with
-            | :? FileNotFoundException -> Error $"\"{path}\" was not found."
-            | :? JsonException -> Error $"Could not parse JSON in \"{path}\" to settings."
-            | e -> Error $"Settings unexpectedly could not be read from \"{path}\": {e.Message}"
+            let verify (settings:UserSettings) =
+                let isEmpty str = String.IsNullOrWhiteSpace str
+                let dirMissing str = not <| (Directory.Exists str)
 
-    [<CompiledName("WriteFile")>]
-    let private writeFile settings filePath =
-        let unicodeEncoder = JavaScriptEncoder.Create(UnicodeRanges.All)
-        let writeIndented = true
-        let options = JsonSerializerOptions(WriteIndented = writeIndented, Encoder = unicodeEncoder)
-        let (FilePath file) = filePath
+                match settings with
+                | { WorkingDirectory = w } when isEmpty w -> Error $"No working directory was specified."
+                | { WorkingDirectory = w } when dirMissing w -> Error $"Working directory \"{w}\" is missing."
+                | { MoveToDirectory = m } when isEmpty m -> Error $"No move-to directory was specified."
+                | { MoveToDirectory = m } when dirMissing m -> Error $"Move-to directory \"{m}\" is missing."
+                | _ -> Ok settings
 
-        try
-            let json = JsonSerializer.Serialize(settings, options)
-            File.WriteAllText(file, json)
-            Ok $"A new default settings file was created at \"{file}\".\nPlease populate it with your desired settings."
-        with
-            | :? FileNotFoundException -> Error $"\"{file}\" was not found."
-            | :? JsonException -> Error $"Failure parsing user settings to JSON."
-            | e -> Error $"Failure writing settings to \"{file}\": {e.Message}"
+            try
+                path
+                |> File.ReadAllText
+                |> JsonSerializer.Deserialize<UserSettings>
+                |> verify
+            with
+                | :? FileNotFoundException -> Error $"\"{path}\" was not found."
+                | :? JsonException -> Error $"Could not parse JSON in \"{path}\" to settings."
+                | e -> Error $"Settings unexpectedly could not be read from \"{path}\": {e.Message}"
 
-    [<CompiledName("WriteDefaultFile")>]
-    let writeDefaultFile (filePath: FilePath option) =
-        let safePath =
-            let defaultFileName = "settings.json"
-            match filePath with
-            | Some p -> p
-            | None -> FilePath <| Path.Combine(AppContext.BaseDirectory, defaultFileName);
-        let defaultSettings =
-            { WorkingDirectory = String.Empty
-              MoveToDirectory = String.Empty
-              HistoryFile = String.Empty
-              HistoryDisplayCount = 25uy // byte
-              SplitChapters = true
-              SleepSecondsBetweenDownloads = 10us // uint16
-              SleepSecondsBetweenBatches = 20us // uint16
-              VerboseOutput = true
-              IgnoreUploadYearUploaders = [||] }
-        writeFile defaultSettings safePath
+        [<CompiledName("WriteFile")>]
+        let private writeFile settings filePath =
+            let unicodeEncoder = JavaScriptEncoder.Create(UnicodeRanges.All)
+            let writeIndented = true
+            let options = JsonSerializerOptions(WriteIndented = writeIndented, Encoder = unicodeEncoder)
+            let (FilePath file) = filePath
+
+            try
+                let json = JsonSerializer.Serialize(settings, options)
+                File.WriteAllText(file, json)
+                Ok $"A new default settings file was created at \"{file}\".\nPlease populate it with your desired settings."
+            with
+                | :? FileNotFoundException -> Error $"\"{file}\" was not found."
+                | :? JsonException -> Error $"Failure parsing user settings to JSON."
+                | e -> Error $"Failure writing settings to \"{file}\": {e.Message}"
+
+        [<CompiledName("WriteDefaultFile")>]
+        let writeDefaultFile (filePath: FilePath option) =
+            let safePath =
+                let defaultFileName = "settings.json"
+                match filePath with
+                | Some p -> p
+                | None -> FilePath <| Path.Combine(AppContext.BaseDirectory, defaultFileName);
+            let defaultSettings =
+                { WorkingDirectory = String.Empty
+                  MoveToDirectory = String.Empty
+                  HistoryFile = String.Empty
+                  HistoryDisplayCount = 25uy // byte
+                  SplitChapters = true
+                  SleepSecondsBetweenDownloads = 10us
+                  SleepSecondsBetweenBatches = 20us
+                  VerboseOutput = true
+                  IgnoreUploadYearUploaders = [||] }
+            writeFile defaultSettings safePath
