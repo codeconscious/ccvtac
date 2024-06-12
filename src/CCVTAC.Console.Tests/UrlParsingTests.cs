9affc72259c170f8f50458f69287cf42c68426fa
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2024-06-12 17:47:04 +0900

    Use regular Lists in tests

--- src/CCVTAC.Console.Tests/UrlParsingTests.cs
+++ src/CCVTAC.Console.Tests/UrlParsingTests.cs
@@ -1,4 +1,4 @@
-using System.Collections.Immutable;
+using System.Collections.Generic;
 using CCVTAC.Console;
 
 namespace CCVTAC.Tests;
@@ -8,8 +8,8 @@ public sealed class UrlParsingTests
     [Fact]
     public void MultipleValidUrlsEntered_CorrectlyParsed()
     {
-        ImmutableList<string> combined = ["https://youtu.be/5OpuZHsPBhQhttps://youtu.be/NT22EGxTuNw"];
-        ImmutableList<string> expected = ["https://youtu.be/5OpuZHsPBhQ", "https://youtu.be/NT22EGxTuNw"];
+        List<string> combined = ["https://youtu.be/5OpuZHsPBhQhttps://youtu.be/NT22EGxTuNw"];
+        List<string> expected = ["https://youtu.be/5OpuZHsPBhQ", "https://youtu.be/NT22EGxTuNw"];
         var actual = UrlHelper.SplitCombinedUrls(combined);
         Assert.Equal(expected.Count, actual.Count);
         Assert.Equal(expected[0], actual[0]);
