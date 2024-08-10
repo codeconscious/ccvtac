c82f2d92299b44a7bf8682b4d827f5f81fd3aa06
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2024-08-10 16:41:29 +0900

    Remove mentions of audio normalization (#60)
    * Use generated regex; style updates
    
    * Make PostProcessor class static
    
    * Remove mentions of audio normalization

--- src/CCVTAC.Console/PostProcessing/Normalizer.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CCVTAC.Console.PostProcessing;
-
-internal static class AudioNormalizer
-{
-    internal static void Run(string workingDirectory, Printer printer)
-    {
-        // ExternalTools.AudioNormalization(workingDirectory, printer);
-    }
-}
