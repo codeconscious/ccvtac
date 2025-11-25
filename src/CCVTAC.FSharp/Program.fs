e615dd2c255c1ca88ce80959e3e0a1db2d5c280a
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2025-11-25 22:20:25 +0900

    Add exit code type to Program.fs

--- src/CCVTAC.FSharp/Program.fs
+++ src/CCVTAC.FSharp/Program.fs
@@ -1,17 +1,12 @@
 namespace CCVTAC.Console
 
 open System
-open System.Linq
 open Spectre.Console
+open CCVTAC.Console
 open CCVTAC.Console.IoUtilities
 open CCVTAC.Console.Settings
 open CCVTAC.Console.Settings.Settings
 open ExtensionMethods
-open CCVTAC.Console
-open CCVTAC.Console.Settings
-open CCVTAC.Console.Settings.Settings.IO
-open CCVTAC.Console.Settings.Settings.LiveUpdating
-open CCVTAC.Console.Settings.Settings.Validation
 
 module Program =
 
@@ -19,17 +14,22 @@ module Program =
     let private settingsFileFlags = [| "-s"; "--settings" |]
     let private defaultSettingsFileName = "settings.json"
 
+    type ExitCodes =
+        | Success = 0
+        | ArgError = 1
+        | OperationError = 2
+
     [<EntryPoint>]
-    let main (args: string[]) : int =
+    let main (args: string array) : int =
         let printer = Printer(showDebug = true)
 
         if args.Length > 0 && caseInsensitiveContains helpFlags args[0] then
-            Help.Print(printer)
-            0
+            Help.Print printer
+            int ExitCodes.Success
         else
             let maybeSettingsPath =
                 if args.Length >= 2 && caseInsensitiveContains settingsFileFlags args[0] then
-                    args.[1] // expected to be a settings file path
+                    args[1] // Expected to be a settings file path
                 else
                     defaultSettingsFileName
 
@@ -37,8 +37,8 @@ module Program =
             let readResult = Settings.IO.read (FilePath maybeSettingsPath)
             match readResult with
             | Error e ->
-                printer.Error(e)
-                1
+                printer.Error e
+                int ExitCodes.ArgError
             // | Ok None ->
             //     // A new settings file was created; nothing more to do
             //     0
@@ -47,7 +47,7 @@ module Program =
                 printer.ShowDebug(not settings.QuietMode)
 
                 // Catch Ctrl-C (SIGINT)
-                Console.CancelKeyPress.Add(fun args ->
+                Console.CancelKeyPress.Add(fun _ ->
                     printer.Warning("\nQuitting at user's request.")
 
                     match Directories.warnIfAnyFiles settings.WorkingDirectory 10 with
@@ -56,18 +56,16 @@ module Program =
                         printer.FirstError(warnResult)
                         match Directories.askToDeleteAllFiles settings.WorkingDirectory printer with
                         | Ok deletedCount -> printer.Info $"%d{deletedCount} file(s) deleted."
-                        | Error delErr -> printer.FirstError(delErr)
-                    // Do not set args.Cancel here; let default behavior terminate the process
+                        | Error delErr -> printer.FirstError delErr
                 )
 
-                // Top-level try to catch unexpected exceptions and report them
                 try
                     Orchestrator.start settings printer
-                    0
+                    int ExitCodes.Success
                 with ex ->
                     printer.Critical $"Fatal error: %s{ex.Message}"
                     AnsiConsole.WriteException(ex)
                     printer.Info(
                         "Please help improve this tool by reporting this error and any relevant URLs at https://github.com/codeconscious/ccvtac/issues."
                     )
-                    1
+                    int ExitCodes.OperationError
