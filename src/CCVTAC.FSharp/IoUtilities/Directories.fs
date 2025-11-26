58cfc088b048b4b4297eb6986c83e22b56901615
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2025-11-26 22:13:50 +0900

    More Directories updates

--- src/CCVTAC.FSharp/IoUtilities/Directories.fs
+++ src/CCVTAC.FSharp/IoUtilities/Directories.fs
@@ -13,23 +13,65 @@ module Directories =
 
     /// Counts the number of audio files in a directory
     let internal audioFileCount (directory: string) =
-        Directory.GetFiles directory
-        |> Array.filter (fun f ->
-            AudioExtensions
-            |> List.exists (fun ext ->
-                // let f' = match Path.GetExtension (f: string) with Null -> "" | NonNull (x: string) -> x // TODO: Improve.
-                // let ext' = match Path.GetExtension (ext: string) with Null -> "" | NonNull (x: string) -> x // TODO: Improve.
-                // Path.GetExtension(f).Equals(ext, StringComparison.OrdinalIgnoreCase) // Error occurs here.
-                StringComparer.OrdinalIgnoreCase.Equals(Path.GetExtension(f), ext)
-            )
-        )
-        |> Array.length
+        DirectoryInfo(directory).EnumerateFiles()
+        |> Seq.filter (fun f -> caseInsensitiveContains AudioExtensions f.Extension)
+        |> Seq.length
 
-    /// Warns if any files are present in the directory
+    /// Returns the filenames in a given directory, optionally ignoring specific filenames
+    let private getDirectoryFileNames
+        (directoryName: string)
+        (customIgnoreFiles: string seq option) =
+
+        let ignoreFiles =
+            customIgnoreFiles
+            |> Option.defaultValue Seq.empty
+            |> Seq.distinct
+            |> Seq.toArray
+
+        Directory.GetFiles(directoryName, AllFilesSearchPattern, enumerationOptions)
+        |> Array.filter (fun filePath -> not (ignoreFiles |> Array.exists filePath.EndsWith))
+
+    /// Deletes all files in the working directory
+    let internal deleteAllFiles (workingDirectory: string) (showMaxErrors: int) =
+        let fileNames = getDirectoryFileNames workingDirectory None
+
+        let mutable successCount = 0
+        let errors = ResizeArray<string>() // TODO: Use an F# list.
+
+        fileNames |> Array.iter (fun fileName ->
+            try
+                File.Delete fileName
+                successCount <- successCount + 1
+            with
+            | ex -> errors.Add ex.Message
+        )
+
+        if errors.Count = 0 then
+            Ok successCount
+        else
+            let output = StringBuilder(
+                $"While {successCount} files were deleted successfully, some files could not be deleted:"
+            )
+
+            errors
+            |> Seq.truncate showMaxErrors
+            |> Seq.iter (fun error ->
+                output.AppendLine($"• {error}") |> ignore
+            )
+
+            if errors.Count > showMaxErrors then
+                output.AppendLine($"... plus {errors.Count - showMaxErrors} more.") |> ignore
+
+            Error (output.ToString())
+
+    /// Asks user if they want to delete all files
+    let internal askToDeleteAllFiles (workingDirectory: string) (printer: Printer) =
+        if printer.AskToBool("Delete all temporary files?", "Yes", "No")
+        then deleteAllFiles workingDirectory 10
+        else Error "Will not delete the files."
+
     let internal warnIfAnyFiles (directory: string) (showMax: int) =
-        let fileNames =
-            Directory.GetFiles(directory, AllFilesSearchPattern, enumerationOptions)
-            |> Array.map Path.GetFileName
+        let fileNames = getDirectoryFileNames directory None
 
         if fileNames.Length = 0 then
             Ok()
@@ -53,60 +95,3 @@ module Directories =
             report.AppendLine("This generally occurs due to the same video appearing twice in playlists.") |> ignore
 
             Error (report.ToString())
-
-    /// Deletes all files in the working directory
-    let internal deleteAllFiles (workingDirectory: string) (showMaxErrors: int) =
-        let fileNames =
-            Directory.GetFiles(workingDirectory, AllFilesSearchPattern, enumerationOptions)
-
-        let mutable successCount = 0
-        let errors = ResizeArray<string>()
-
-        fileNames |> Array.iter (fun fileName ->
-            try
-                File.Delete fileName
-                successCount <- successCount + 1
-            with
-            | ex -> errors.Add(ex.Message)
-        )
-
-        if errors.Count = 0 then
-            Ok successCount
-        else
-            let output = StringBuilder(
-                $"While {successCount} files were deleted successfully, some files could not be deleted:"
-            )
-
-            errors
-            |> Seq.truncate showMaxErrors
-            |> Seq.iter (fun error ->
-                output.AppendLine($"• {error}") |> ignore
-            )
-
-            if errors.Count > showMaxErrors then
-                output.AppendLine($"... plus {errors.Count - showMaxErrors} more.") |> ignore
-
-            Error (output.ToString())
-
-    /// Asks user if they want to delete all files
-    let internal askToDeleteAllFiles (workingDirectory: string) (printer: Printer) =
-        let doDelete = printer.AskToBool("Delete all temporary files?", "Yes", "No")
-
-        if doDelete
-        then deleteAllFiles workingDirectory 10
-        else Error "Will not delete the files."
-
-    /// Returns the filenames in a given directory, optionally ignoring specific filenames
-    let private getDirectoryFileNames
-        (directoryName: string)
-        (customIgnoreFiles: string seq option) =
-
-        let ignoreFiles =
-            customIgnoreFiles
-            |> Option.defaultValue Seq.empty
-            |> Seq.distinct
-            |> Seq.toArray
-
-        Directory.GetFiles(directoryName, AllFilesSearchPattern, enumerationOptions)
-        |> Array.filter (fun filePath -> not (ignoreFiles |> Array.exists filePath.EndsWith))
-
