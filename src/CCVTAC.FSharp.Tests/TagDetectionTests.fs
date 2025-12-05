110be8756eb3726f81bf62d472b47aa4be050fee
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2025-12-05 12:35:40 +0900

    Add more test cases

--- src/CCVTAC.FSharp.Tests/TagDetectionTests.fs
+++ src/CCVTAC.FSharp.Tests/TagDetectionTests.fs
@@ -1,11 +1,10 @@
-// module CCVTAC.FSharp.Tests.TaggingTests
 module TagDetectionTests
 
-open Xunit
 open CCVTAC.Console.PostProcessing.Tagging
 open CCVTAC.Console.PostProcessing
 open CCVTAC.Console.Settings.Settings
 open System
+open Xunit
 
 let emptyVideoMetadata = {
     Id = String.Empty
@@ -14,24 +13,24 @@ let emptyVideoMetadata = {
     Description = String.Empty
     ChannelId = String.Empty
     ChannelUrl = String.Empty
-    Duration = System.Nullable 0
-    ViewCount = System.Nullable 0
-    AgeLimit = System.Nullable 0
+    Duration = Nullable 0
+    ViewCount = Nullable 0
+    AgeLimit = Nullable 0
     WebpageUrl = String.Empty
     Categories = []
     Tags = []
-    PlayableInEmbed = Nullable<bool> false
+    PlayableInEmbed = Nullable false
     LiveStatus = String.Empty
-    ReleaseTimestamp = System.Nullable 0
+    ReleaseTimestamp = Nullable 0
     FormatSortFields = []
     Album = String.Empty
     Artist = String.Empty
     Track = String.Empty
-    CommentCount = System.Nullable 0
-    LikeCount = System.Nullable 0
+    CommentCount = Nullable 0
+    LikeCount = Nullable 0
     Channel = String.Empty
-    ChannelFollowerCount = System.Nullable 0
-    ChannelIsVerified = Nullable<bool> false
+    ChannelFollowerCount = Nullable 0
+    ChannelIsVerified = Nullable false
     Uploader = String.Empty
     UploaderId = String.Empty
     UploaderUrl = String.Empty
@@ -43,31 +42,31 @@ let emptyVideoMetadata = {
     WebpageUrlDomain = String.Empty
     Extractor = String.Empty
     ExtractorKey = String.Empty
-    PlaylistCount = System.Nullable 0
+    PlaylistCount = Nullable 0
     Playlist = String.Empty
     PlaylistId = String.Empty
     PlaylistTitle = String.Empty
-    NEntries = System.Nullable 0
-    PlaylistIndex = Nullable<uint32> 0u
+    NEntries = Nullable 0
+    PlaylistIndex = Nullable 0u
     DisplayId = String.Empty
     Fulltitle = String.Empty
     DurationString = String.Empty
     ReleaseDate = String.Empty
-    ReleaseYear = Nullable<uint32> 0u
-    IsLive = Nullable<bool> false
-    WasLive = Nullable<bool> false
-    Epoch = System.Nullable 0
-    Asr = System.Nullable 0
-    Filesize = System.Nullable 0
+    ReleaseYear = Nullable 0u
+    IsLive = Nullable false
+    WasLive = Nullable false
+    Epoch = Nullable 0
+    Asr = Nullable 0
+    Filesize = Nullable 0
     FormatId = String.Empty
     FormatNote = String.Empty
-    SourcePreference = System.Nullable 0
-    AudioChannels = System.Nullable 0
-    Quality = Nullable<double> 0
-    HasDrm = Nullable<bool> false
-    Tbr = Nullable<double> 0
+    SourcePreference = Nullable 0
+    AudioChannels = Nullable 0
+    Quality = Nullable 0
+    HasDrm = Nullable false
+    Tbr = Nullable 0
     Url = String.Empty
-    LanguagePreference = System.Nullable 0
+    LanguagePreference = Nullable 0
     Ext = String.Empty
     Vcodec = String.Empty
     Acodec = String.Empty
@@ -76,35 +75,85 @@ let emptyVideoMetadata = {
     Resolution = String.Empty
     AudioExt = String.Empty
     VideoExt = String.Empty
-    Vbr = System.Nullable 0
-    Abr = Nullable<double> 0
+    Vbr = Nullable 0
+    Abr = Nullable 0
     Format = String.Empty
     Type = String.Empty
 }
 
+let newLine = Environment.NewLine
+
 [<Fact>]
 let ``Detects album name in video description`` () =
-    let testAlbumName = "Test Album Name"
-    let videoMetadata = { emptyVideoMetadata with Description = $" album: {testAlbumName}" }
-    let fallback : string option = None
+    let testArtist = "Test Artist Name (日本語入り）"
+    let testAlbum = "Test Album Name (日本語入り）"
+    let testTitle = "Test Title (日本語入り）"
+    let testComposer = "Test Composer (日本語入り）"
+    let testYear = 1945u
 
-    let detectionPattern : TagDetectionPattern = {
+    let videoMetadata = {
+        emptyVideoMetadata with
+            Title = $"{testArtist}「{testTitle}」"
+            Description = $"album: {testAlbum}{newLine}℗ %d{testYear}{newLine}Composed by: {testComposer}" }
+
+    let artistPattern = {
+        RegexPattern = "^(.+?)「(.+)」"
+        MatchGroup = 1
+        SearchField = "title"
+        Summary = Some "Find artist in video title"
+    }
+
+    let titlePattern = {
+        artistPattern with
+            MatchGroup = 2
+            Summary = Some "Find title in the video title"
+    }
+
+    let albumPattern = {
         RegexPattern = "(?<=[Aa]lbum: ).+"
         MatchGroup = 0
         SearchField = "description"
-        Summary = Some "Find album name in description"
+        Summary = Some "Find album in description"
     }
 
-    let tagDetectionPatterns : TagDetectionPatterns = {
-        Title = [||]
-        Artist = [||]
-        Album = [| detectionPattern |]
-        Composer = [||]
-        Year = [||]
+    let composerPattern = {
+        RegexPattern = "(?<=[Cc]omposed by |[Cc]omposed by: |[Cc]omposer: |作曲[:：・]).+"
+        MatchGroup = 0
+        SearchField = "description"
+        Summary = Some "Find composer in description"
     }
 
-    let result = TagDetection.detectAlbum videoMetadata fallback tagDetectionPatterns
+    let yearPattern = {
+        RegexPattern = "(?<=℗ )[12]\d{3}"
+        MatchGroup = 0
+        SearchField = "description"
+        Summary = Some "Find year in description"
+    }
 
-    match result with
-    | Some r -> Assert.Equal(testAlbumName, r)
-    | None -> Assert.Fail $"Expected album name \"{testAlbumName}\" was not found"
+    let tagDetectionPatterns = {
+        Title = [| titlePattern |]
+        Artist = [| artistPattern  |]
+        Album = [| albumPattern |]
+        Composer = [| composerPattern |]
+        Year = [| yearPattern |]
+    }
+
+    match TagDetection.detectArtist videoMetadata None tagDetectionPatterns with
+    | Some artistResult -> Assert.Equal(testArtist, artistResult)
+    | None -> Assert.Fail $"Expected artist \"{testArtist}\" was not found."
+
+    match TagDetection.detectAlbum videoMetadata None tagDetectionPatterns with
+    | Some albumResult -> Assert.Equal(testAlbum, albumResult)
+    | None -> Assert.Fail $"Expected album \"{testAlbum}\" was not found."
+
+    match TagDetection.detectTitle videoMetadata None tagDetectionPatterns with
+    | Some titleResult -> Assert.Equal(testTitle, titleResult)
+    | None -> Assert.Fail $"Expected title \"{testTitle}\" was not found."
+
+    match TagDetection.detectComposers videoMetadata tagDetectionPatterns with
+    | Some composerResult -> Assert.Equal(testComposer, composerResult)
+    | None -> Assert.Fail $"Expected composer \"{testComposer}\" was not found."
+
+    match TagDetection.detectReleaseYear videoMetadata None tagDetectionPatterns with
+    | Some yearResult -> Assert.Equal(testYear, yearResult)
+    | None -> Assert.Fail $"Expected year \"%d{testYear}\" was not found."
