53502ffde4d394f97c70db8e9bc971b4ce18a443
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2026-01-01 21:14:07 +0900

    Add multiple-failed-validations test

--- src/CCVTAC.Tests/PostProcessing/Tagging/TaggingSetTests.fs
+++ src/CCVTAC.Tests/PostProcessing/Tagging/TaggingSetTests.fs
@@ -153,20 +153,82 @@ module TaggingSetInstantiationTests =
         Assert.Equal(expected, actual)
 
     [<Fact>]
+    let ``fails with multiple errors when multiple files from playlists are missing metadata`` () =
+        let dir = Path.Combine("user", "Downloads", "Audio", "tmp")
+
+        // Valid, so is not included in the error output.
+        let videoId1 = "__FuYW30t7E"
+        let fileNameBase1 = $"""{Path.Combine(dir, "Video Name 1")} [{videoId1}]"""
+        let audioFile1 = $"{fileNameBase1}.m4a"
+        let jsonFile1 = $"{fileNameBase1}.info.json"
+        let imageFile1 = $"{fileNameBase1}.jpg"
+
+        // Missing audio file.
+        let videoId2 = "G_d242400EF"
+        let fileNameBase2 = $"""{Path.Combine(dir, "Video Name 2")} [{videoId2}]"""
+        let jsonFile2 = $"{fileNameBase2}.info.json"
+        let imageFile2 = $"{fileNameBase2}.jpg"
+
+        // Missing JSON file.
+        let videoId3 = "048_702850G"
+        let fileNameBase3 = $"""{Path.Combine(dir, "Video Name 3")} [{videoId3}]"""
+        let audioFile3 = $"{fileNameBase3}.m4a"
+        let imageFile3 = $"{fileNameBase3}.jpg"
+
+        // Missing image file.
+        let videoId4 = "048_702850H"
+        let fileNameBase4 = $"""{Path.Combine(dir, "Video Name 4")} [{videoId4}]"""
+        let audioFile4 = $"{fileNameBase4}.m4a"
+        let jsonFile4 = $"{fileNameBase4}.info.json"
+
+        // Missing audio and JSON file.
+        let videoId5 = "048_702850I"
+        let fileNameBase5 = $"""{Path.Combine(dir, "Video Name 5")} [{videoId5}]"""
+        let imageFile5 = $"{fileNameBase5}.jpg"
+
+        // Missing audio and image file.
+        let videoId6 = "048_702850J"
+        let fileNameBase6 = $"""{Path.Combine(dir, "Video Name 6")} [{videoId6}]"""
+        let jsonFile6 = $"{fileNameBase6}.info.json"
+
+        let playlistId = "OLAK5uy_ljoU26IjmfmI__eBcG_r0GzH-K3GaJy3s"
+        let playlistNameBase = $"""{Path.Combine(dir, "Playlist Name")} [{playlistId}]"""
+        let playlistJsonFile = $"{playlistNameBase}.info.json"
+        let playlistImageFile = $"{playlistNameBase}.jpg"
+
+        let expected : Result<TaggingSet list, string list list> =
+            Error
+              [["No supported audio files were found by extension."]
+               ["No JSON file was found."]
+               ["No image file was found."]
+               ["No supported audio files were found by extension."
+                "No JSON file was found."]
+               ["No supported audio files were found by extension."
+                "No image file was found."]]
+
+        let files = [
+            audioFile1; jsonFile1; imageFile1
+            jsonFile2; imageFile2
+            audioFile3; imageFile3
+            audioFile4; jsonFile4
+            imageFile5
+            jsonFile6
+            playlistJsonFile; playlistImageFile
+        ]
+        let actual = createSets files
+
+        Assert.Equal(expected, actual)
+
+
+    [<Fact>]
     let ``parses split video files`` () =
-        (*
-            How to Use YouTube Chapters - YouTube Video Sections - 001 Introduction [6-0cERIVsFg].m4a
-            How to Use YouTube Chapters - YouTube Video Sections - 002 How to Add Chapters to a YouTube Video [6-0cERIVsFg].m4a
-            How to Use YouTube Chapters - YouTube Video Sections - 003 How to Use Chapters as a Viewer [6-0cERIVsFg].m4a
-            How to Use YouTube Chapters - YouTube Video Sections [6-0cERIVsFg].info.json
-            How to Use YouTube Chapters - YouTube Video Sections [6-0cERIVsFg].jpg
-        *)
         let dir = Path.Combine("user", "Downloads", "Audio", "tmp")
 
         let videoId = "__FuYW30t7E"
         let audioFiles =
-            [1..9]
-            |> List.mapi (fun i x -> $"""{Path.Combine(dir, $"Video Name - 00{i} Name of Chapter {i}")} [{videoId}].m4a""")
+            List.mapi
+                (fun i _ -> $"""{Path.Combine(dir, $"Video Name - 00{i} Name of Chapter {i}")} [{videoId}].m4a""")
+                [1..9]
         let fileNameBase = $"""{Path.Combine(dir, "Video Name")} [{videoId}]"""
         let jsonFile = $"{fileNameBase}.info.json"
         let imageFile = $"{fileNameBase}.jpg"
