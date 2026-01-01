3337978e859cbec252c066acdb3c73bbe2b9a64a
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2026-01-01 16:37:20 +0900

    Add more tests

--- src/CCVTAC.Tests/PostProcessing/Tagging/TaggingSetTests.fs
+++ src/CCVTAC.Tests/PostProcessing/Tagging/TaggingSetTests.fs
@@ -1,10 +1,7 @@
 module TaggingSetTests
 
-open CCVTAC.Main.PostProcessing.Tagging
 open CCVTAC.Main.PostProcessing.Tagging.TaggingSets
-open System
 open System.IO
-open System.Text
 open Xunit
 
 module TaggingSetInstantiationTests =
@@ -18,7 +15,35 @@ module TaggingSetInstantiationTests =
         let jsonFile = $"{fileNameBase}.info.json"
         let imageFile = $"{fileNameBase}.jpg"
 
+        let expected : Result<TaggingSet list, string list list> =
+          Ok [
+              {
+                ResourceId = videoId
+                AudioFilePaths = [audioFile]
+                JsonFilePath = jsonFile
+                ImageFilePath = imageFile
+              }
+          ]
+
         let files = [audioFile; jsonFile; imageFile]
+        let actual = createSets files
+
+        Assert.Equal(expected, actual)
+
+    [<Fact>]
+    let ``parses playlist files`` () =
+        let dir = Path.Combine("user", "Downloads", "Audio", "tmp")
+
+        let videoId = "__FuYW30t7E"
+        let fileNameBase = $"""{Path.Combine(dir, "Video Name")} [{videoId}]"""
+        let audioFile = $"{fileNameBase}.m4a"
+        let jsonFile = $"{fileNameBase}.info.json"
+        let imageFile = $"{fileNameBase}.jpg"
+
+        let playlistId = "OLAK5uy_ljoU26IjmfmI__eBcG_r0GzH-K3GaJy3s"
+        let playlistNameBase = $"""{Path.Combine(dir, "Playlist Name")} [{playlistId}]"""
+        let playlistJsonFile = $"{playlistNameBase}.info.json"
+        let playlistImageFile = $"{playlistNameBase}.jpg"
 
         let expected : Result<TaggingSet list, string list list> =
           Ok [
@@ -30,6 +55,101 @@ module TaggingSetInstantiationTests =
               }
           ]
 
+        let files = [audioFile; jsonFile; imageFile; playlistJsonFile; playlistImageFile]
+        let actual = createSets files
+
+        Assert.Equal(expected, actual)
+
+    [<Fact>]
+    let ``parses multiple files from playlist`` () =
+        let dir = Path.Combine("user", "Downloads", "Audio", "tmp")
+
+        let videoId1 = "__FuYW30t7E"
+        let fileNameBase1 = $"""{Path.Combine(dir, "Video Name 1")} [{videoId1}]"""
+        let audioFile1 = $"{fileNameBase1}.m4a"
+        let jsonFile1 = $"{fileNameBase1}.info.json"
+        let imageFile1 = $"{fileNameBase1}.jpg"
+
+        let videoId2 = "G_d242400EF"
+        let fileNameBase2 = $"""{Path.Combine(dir, "Video Name 2")} [{videoId2}]"""
+        let audioFile2 = $"{fileNameBase2}.m4a"
+        let jsonFile2 = $"{fileNameBase2}.info.json"
+        let imageFile2 = $"{fileNameBase2}.jpg"
+
+        let videoId3 = "048_702850G"
+        let fileNameBase3 = $"""{Path.Combine(dir, "Video Name 2")} [{videoId3}]"""
+        let audioFile3 = $"{fileNameBase3}.m4a"
+        let jsonFile3 = $"{fileNameBase3}.info.json"
+        let imageFile3 = $"{fileNameBase3}.jpg"
+
+        let playlistId = "OLAK5uy_ljoU26IjmfmI__eBcG_r0GzH-K3GaJy3s"
+        let playlistNameBase = $"""{Path.Combine(dir, "Playlist Name")} [{playlistId}]"""
+        let playlistJsonFile = $"{playlistNameBase}.info.json"
+        let playlistImageFile = $"{playlistNameBase}.jpg"
+
+        let expected : Result<TaggingSet list, string list list> =
+          Ok [
+              {
+                ResourceId = videoId1
+                AudioFilePaths = [audioFile1]
+                JsonFilePath = jsonFile1
+                ImageFilePath = imageFile1
+              }
+              {
+                ResourceId = videoId2
+                AudioFilePaths = [audioFile2]
+                JsonFilePath = jsonFile2
+                ImageFilePath = imageFile2
+              }
+              {
+                ResourceId = videoId3
+                AudioFilePaths = [audioFile3]
+                JsonFilePath = jsonFile3
+                ImageFilePath = imageFile3
+              }
+          ]
+
+        let files = [
+            audioFile1; jsonFile1; imageFile1
+            audioFile2; jsonFile2; imageFile2
+            audioFile3; jsonFile3; imageFile3
+            playlistJsonFile; playlistImageFile
+        ]
+        let actual = createSets files
+
+        Assert.Equal(expected, actual)
+
+    [<Fact>]
+    let ``parses split video files`` () =
+        (*
+            How to Use YouTube Chapters - YouTube Video Sections - 001 Introduction [6-0cERIVsFg].m4a
+            How to Use YouTube Chapters - YouTube Video Sections - 002 How to Add Chapters to a YouTube Video [6-0cERIVsFg].m4a
+            How to Use YouTube Chapters - YouTube Video Sections - 003 How to Use Chapters as a Viewer [6-0cERIVsFg].m4a
+            How to Use YouTube Chapters - YouTube Video Sections [6-0cERIVsFg].info.json
+            How to Use YouTube Chapters - YouTube Video Sections [6-0cERIVsFg].jpg
+        *)
+        let dir = Path.Combine("user", "Downloads", "Audio", "tmp")
+
+        let videoId = "__FuYW30t7E"
+        let audioFiles =
+            [1..9]
+            |> List.mapi (fun i x -> $"""{Path.Combine(dir, $"Video Name - 00{i} Name of Chapter {i}")} [{videoId}].m4a""")
+        let fileNameBase = $"""{Path.Combine(dir, "Video Name")} [{videoId}]"""
+        let jsonFile = $"{fileNameBase}.info.json"
+        let imageFile = $"{fileNameBase}.jpg"
+
+
+        let expected : Result<TaggingSet list, string list list> =
+          Ok [
+              {
+                ResourceId = videoId
+                AudioFilePaths = audioFiles
+                JsonFilePath = jsonFile
+                ImageFilePath = imageFile
+              }
+          ]
+
+        let files =  audioFiles @ [jsonFile; imageFile ]
         let actual = createSets files
 
         Assert.Equal(expected, actual)
