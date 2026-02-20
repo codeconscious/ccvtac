b63362072c1a348e013847457d93f496607c95d0
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2026-02-20 12:30:46 +0900

    Refactor input splitting

--- src/CCVTAC.Main/InputHelper.fs
+++ src/CCVTAC.Main/InputHelper.fs
@@ -6,7 +6,7 @@ module InputHelper =
 
     let prompt = $"Enter one or more YouTube media URLs or commands (or \"%s{Commands.helpCommand}\"):\n▶︎"
 
-    /// A regular expression that detects the beginnings of URLs and application commands in input strings.
+    /// A regular expression that detects the beginnings of URLs and application commands within input strings.
     let private userInputRegex = Regex(@"(?:https:|\\)", RegexOptions.Compiled)
 
     type private IndexPair = { Start: int; End: int }
@@ -15,7 +15,7 @@ module InputHelper =
 
     type CategorizedInput = { Text: string; Category: InputCategory }
 
-    type CategoryCounts (counts: Map<InputCategory,int>) =
+    type CategoryCounts (counts: Map<InputCategory, int>) =
         member _.Item
             with get (category: InputCategory) =
                 match counts.TryGetValue category with
@@ -26,10 +26,7 @@ module InputHelper =
     let splitInputText input : string list =
         let matches = userInputRegex.Matches input |> Seq.cast<Match> |> Seq.toList
 
-        match matches with
-        | [] -> []
-        | [_] -> [input]
-        | _ ->
+        let split (matches: Match list) =
             let startIndices = matches |> List.map _.Index
 
             let indexPairs : IndexPair list =
@@ -40,25 +37,28 @@ module InputHelper =
                         then input.Length
                         else startIndices[idx + 1]
                     { Start = startIndex
-                      End = endIndex })
+                      End   = endIndex })
 
             indexPairs
-            |> List.map (fun p -> input[p.Start..(p.End - 1)].Trim())
+            |> List.map (fun pair -> input[pair.Start..(pair.End - 1)].Trim())
             |> List.distinct
+
+        match matches with
+        | [] -> []
+        | [_] -> [input]
+        | _ -> split matches
 
     let categorizeInputs inputs : CategorizedInput list =
         inputs
         |> List.map (fun input ->
             { Text = input
-              Category = if input.StartsWith(string Commands.prefix)
+              Category = if input[0] = Commands.prefix
                          then InputCategory.Command
                          else InputCategory.Url })
 
     let countCategories (inputs: CategorizedInput list) : CategoryCounts =
-        let counts =
-            inputs
-            |> List.groupBy _.Category
-            |> List.map (fun (k, grp) -> k, grp |> Seq.length)
-            |> Map.ofSeq
-
-        CategoryCounts counts
+        inputs
+        |> List.groupBy _.Category
+        |> List.map (fun (k, grp) -> k, grp |> Seq.length)
+        |> Map.ofSeq
+        |> CategoryCounts
