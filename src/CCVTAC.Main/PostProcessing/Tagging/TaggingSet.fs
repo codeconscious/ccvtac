00c6fefe4f446f72502bb2fc01575de687893b4e
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2026-04-17 21:53:50 +0900

    Update operator

--- src/CCVTAC.Main/PostProcessing/Tagging/TaggingSet.fs
+++ src/CCVTAC.Main/PostProcessing/Tagging/TaggingSet.fs
@@ -2,6 +2,7 @@ namespace CCVTAC.Main.PostProcessing.Tagging
 
 open CCVTAC.Main.IoUtilities
 open CCFSharpUtils
+open CCFSharpUtils.Operators
 open FsToolkit.ErrorHandling
 open System.IO
 open System.Text.RegularExpressions
@@ -89,4 +90,4 @@ module TaggingSet =
             |> List.mapSnd fileName
             |> List.map createValidated
             |> List.sequenceResultA
-            |! List.collect id
+            |!! List.collect id
